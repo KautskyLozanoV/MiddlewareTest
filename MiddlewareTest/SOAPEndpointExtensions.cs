ff7daee3797317a946ee8a2dc54f45718b0a9817
Kautsky Lozano <kautskylozanov@outlook.com>
2019-09-17 18:49:15 +0200

    First commit

---
+++ MiddlewareTest/SOAPEndpointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using middlewareTest;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class SOAPEndpointExtensions
+    {
+        public static IApplicationBuilder UseSOAPEndpoint(this IApplicationBuilder builder) {
+            return builder.UseMiddleware<SOAPEndpointMiddleware>();
+        }
+    }
+}
---
+++ MiddlewareTest/SOAPEndpointMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace middlewareTest
+{
+    public class SOAPEndpointMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public SOAPEndpointMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext) {
+            Contract.Ensures(httpContext != null);
+            Console.WriteLine($"Request for {httpContext.Request.Path} received ({httpContext.Request.ContentLength ?? 0} bytes)");
+
+            // Call the next middleware delegate in the pipeline 
+            await _next.Invoke(httpContext);
+        }
+    }
+}
